b3fa453b0d17c733af9311ccb50788d259441c33
huzaifa3165 <83898816+huzaifa3165@users.noreply.github.com>
2022-10-09 13:37:34 +0500

    created cart item from redux state

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,17 @@
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
 import { connect } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropDown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       {console.log(cartItems)}
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem item={cartItem} key={cartItem.id} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
